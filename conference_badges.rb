784d87da89918febb0c1a326b5b63e414e484fb5
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-09-05 14:16:09 +0100

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker (name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (names)
+  @badge = []
+  names.each do |name|
+    @badge << badge_maker(name)
+  end
+  @badge
+end
+
+def assign_rooms (names)
+  index = 1
+  @assigned_rooms = []
+  names.each do |name|
+    @assigned_rooms << "Hello, #{name}! You'll be assigned to room #{index}!"
+    index += 1
+  end
+  @assigned_rooms
+end
+
+def printer (names)
+  batch_badge_creator (names)
+  @badge.each do |i|
+    puts i
+  end
+  assign_rooms (names)
+  @assigned_rooms.each do |i|
+    puts i
+  end
+end
